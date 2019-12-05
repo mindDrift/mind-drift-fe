884f2aadc7da6015173a5547e90100600a116fa6
adoxic <griffinmaeve@gmail.com>
2019-12-05 14:57:43 -0800

    breath counter works

--- src/components/Breathe.js
+++ src/components/Breathe.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Breathe = ({}) => {
+const Breathe = ({ inhale, exhale, holdIn, holdOut }) => {
   const [counter, setCounter] = useState(0);
+  const [index, setIndex] = useState(0);
+
+  const durationArr = [inhale, holdIn, exhale, holdOut];
+  const actionArr = ['inhale', 'hold', 'exhale', 'hold'];
 
   useEffect(() => {
-
-    if(counter === 3) return;
+    if(counter > durationArr[index]) {
+      setCounter(0);
+      setIndex((index + 1) % 4);
+      return;
+    }
     setTimeout(() => {
       setCounter(counter + 1);
     }, 1000);
@@ -13,10 +21,18 @@ const Breathe = ({}) => {
 
   return (
     <div>
+      <p>{actionArr[index]}</p>
       <p>{counter}</p>
       <button>Done</button>
     </div>
   );
+};
+
+Breathe.propTypes = {
+  inhale: PropTypes.number.isRequired,
+  exhale: PropTypes.number.isRequired,
+  holdIn: PropTypes.number.isRequired,
+  holdOut: PropTypes.number.isRequired
 };
 
 export default Breathe;

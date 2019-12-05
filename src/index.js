08941c724c67bd9d1bc13f4f40d1aeb8bf4dd43f
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-12-05 11:04:21 -0800

    auth0 login working!!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import 'regenerator-runtime/runtime';
+import App from './components/App';
 //import * as serviceWorker from './serviceWorker';
 import { Auth0Provider } from './react-auth0-spa';
 import config from './auth_config.json';

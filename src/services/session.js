453cbadcd35cca7e1a91beb6d688cc1de28ca85c
Dave Trost <davetrost@gmail.com>
2019-12-06 11:49:30 -0800

    cleanup console log

--- src/services/session.js
+++ src/services/session.js
@@ -3,7 +3,6 @@ import { post } from './request';
 const SESSION_URL = 'https://mind-drift-be.herokuapp.com/api/v1';
 
 export const postSession = (start, duration, userId, settings) => {
-  console.log('post', start, duration, userId, settings);
   return post(`${SESSION_URL}/sessions`, { start, duration, userId, settings });
 };
 

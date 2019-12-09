1380d9cc82c0f88655471cd0e212cc28bcef5fbb
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-12-09 11:38:34 -0800

    styling for wrapup component complete

--- src/components/WrapUp.js
+++ src/components/WrapUp.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './WrapUp.css';
 
 const WrapUp = ({ handleSubmit }) => {
 
   return (
-    <>
+    <section className={styles.WrapUp}>
       <p>Session complete ... how do you feel?</p>
       <button onClick={handleSubmit}>Submit</button>
-    </>
+    </section>
   );
 };
 

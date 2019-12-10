ce66b6c13f8b404039026eb437ab5f6e68373746
Dave Trost <davetrost@gmail.com>
2019-12-10 01:27:31 -0800

    random updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,24 @@ import NavBar from './NavBar';
 import styles from './Home.css';
 
 const Home = () => {
+  const divStyle = {
+    position: 'fixed',
+    left: '30%',
+    right: '30%',
+    top: '25%',
+  };
   return (
     <section className={styles.Home}>
       <header>
         <NavBar />
       </header>
+      <div style={divStyle}>logo needed here</div>
       <div className={styles.breatheContainer}>
         <Link to="/breathe">
           <button className={styles.breatheButton}>Breathe</button>
+        </Link>
+        <Link to="/settings">
+          <button className={styles.breatheButton}>Setup</button>
         </Link>
       </div>
     </section>

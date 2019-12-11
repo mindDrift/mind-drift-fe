6a6465c43e24cb3e382d0807ea47033767d4dfda
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-12-11 10:53:04 -0800

    adding edit icon, styling for form beginning

--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './SettingsForm.css';
 
 const SettingsForm = ({ settings, handleSubmit, handleChange }) => {
   const { title, description, inhale, holdIn, exhale, holdOut, endTime } = settings;
-  
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        title:
-        <input onChange={handleChange} type='text' name='title' placeholder='title' value={title} />
-      </label>
-      <label>
-        description:
-        <textarea onChange={handleChange} name='description' placeholder='description' value={description} />
-      </label>
-      <label>
-        Inhale:
-        <input onChange={handleChange} type='number' name='inhale' value={inhale} />
-      </label>
-      <label>
-        Hold Inhale:
-        <input onChange={handleChange} type='number' name='holdIn' value={holdIn} />
-      </label>
-      <label>
-        Exhale:
-        <input onChange={handleChange} type='number' name='exhale' value={exhale} />
-      </label>
-      <label>
-        Hold Exhale:
-        <input onChange={handleChange} type='number' name='holdOut' value={holdOut} />
-      </label>
-      <label>
-        Duration:
-        <input onChange={handleChange} type='number' name='endTime' value={endTime} />
-      </label>
-      <button>Save</button>
-    </form>
+    <section className={styles.Form}>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          title:
+          <input onChange={handleChange} type='text' name='title' placeholder='title' value={title} />
+        </label>
+        <label>
+          description:
+          <textarea onChange={handleChange} name='description' placeholder='description' value={description} />
+        </label>
+        <label>
+          Inhale:
+          <input onChange={handleChange} type='number' name='inhale' value={inhale} />
+        </label>
+        <label>
+          Hold Inhale:
+          <input onChange={handleChange} type='number' name='holdIn' value={holdIn} />
+        </label>
+        <label>
+          Exhale:
+          <input onChange={handleChange} type='number' name='exhale' value={exhale} />
+        </label>
+        <label>
+          Hold Exhale:
+          <input onChange={handleChange} type='number' name='holdOut' value={holdOut} />
+        </label>
+        <label>
+          Duration:
+          <input onChange={handleChange} type='number' name='endTime' value={endTime} />
+        </label>
+        <button>Save</button>
+      </form>
+    </section>
   );
 };
 

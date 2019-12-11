a399cee47bcadc1faf48d68263bcee527b083870
Dave Trost <davetrost@gmail.com>
2019-12-11 12:25:02 -0800

    profile update - no loading needed (for now)

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { useAuth0 } from '../react-auth0-spa';
 import NavBar from './NavBar';
 import styles from './Profile.css';
+import Loading from './Loading';
 
 const Profile = () => {
-  const { loading, user } = useAuth0();
-
-  if(loading || !user) {
-    return <div className={styles.Loading}>Loading...</div>;
-  }
-
+  const { user } = useAuth0();
+  const waitingForData = false;
   return (
     <>
+      <Loading loading={waitingForData} />
       <NavBar />
       <section className={styles.ProfileArea}>
         <img src={user.picture} alt="Profile" />

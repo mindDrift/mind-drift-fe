a2a9eebabea68c686c3677f606ac7f2bbcac1c4b
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-12-11 14:21:45 -0800

    started About Us component

---
+++ src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './AboutUs.css';
+
+const AboutUs () => {
+  return (
+    <section className={styles.AboutUs}>
+
+    </section>
+  );
+}
+
+export default AboutUs;

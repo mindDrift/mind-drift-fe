33d49b5bf303e105acf603615e4071cc188cc088
Dave Trost <davetrost@gmail.com>
2019-12-11 23:54:58 -0800

    styling for achievements items on Profile page

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -23,8 +23,7 @@ const Profile = () => {
       {!loading &&
         <section className={styles.ProfileArea}>
           <img src={user.picture} alt="Profile" />
-          <h2>Your Profile:</h2>
-          <p>Hello, {user.name}</p>
+          <h2>Hello, {user.name}</h2>
           <div>
             <Achievements achieves={achieves} />
           </div>

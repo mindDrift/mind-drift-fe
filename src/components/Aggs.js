ea62e2dee63934ec1814aad0c3201c122299c10a
adoxic <griffinmaeve@gmail.com>
2019-12-12 13:57:21 -0800

    all black to navy, and new icons user page

--- src/components/Aggs.js
+++ src/components/Aggs.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Aggs.css';
+import blueicon from '../assets/blueicon.png';
+import greenicon from '../assets/greenicon.png';
+import peachicon from '../assets/peachicon.png';
 
 const Aggs = ({ average, total, streak }) => {
   return (
@@ -9,27 +12,27 @@ const Aggs = ({ average, total, streak }) => {
       
       <p>
         <div className={styles.badge}>
-          <h1>{average.time}</h1>
+          <h2>{average.time}</h2>
           <h1>{average.type}</h1>
-          <img src='https://raw.githubusercontent.com/mindDrift/mind-drift-fe/dev/src/assets/AVGtimeicon.png' />
+          <img src={blueicon} />
         </div>
         average session
       </p>
      
       <p>
         <div className={styles.badge}>
-          <h1>{total.time}</h1>
+          <h2>{total.time}</h2>
           <h1>{total.type}</h1>
-          <img src='https://raw.githubusercontent.com/mindDrift/mind-drift-fe/dev/src/assets/AVGtimeicon.png' />
+          <img src={greenicon} />
         </div>
         total time breathing
       </p>
       
       <p>
-
         <div className={styles.badge}>
           <h2>{streak}</h2>
-          <img src='https://raw.githubusercontent.com/mindDrift/mind-drift-fe/dev/src/assets/AVGtimeicon.png' />
+          <h1>days</h1>
+          <img src={blueicon} />
         </div>
         current streak
       </p>

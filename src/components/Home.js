e8d1b849bee1632ce8033d53a92a07e75362596a
Dave Trost <davetrost@gmail.com>
2019-12-12 03:43:27 -0800

    implement Toasts from Bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import styles from './Home.css';
+import Toast from 'react-bootstrap/Toast';
 
 const Home = () => {
+  const [show, setShow] = useState(true);
+
   return (
     <>
       <section className={styles.Home}>
         <NavBar />
+        <Toast 
+          onClose={() => setShow(false)} 
+          show={show} 
+          delay={3500}
+          autohide
+          className={styles.Toast}
+        >
+          <img src='https://raw.githubusercontent.com/mindDrift/mind-drift-fe/dev/src/assets/firstStepsicon.png' />
+          <h3>First Steps mockup</h3>
+          <p>You completed your first breathing session</p>
+        </Toast>
         <div className={styles.homeContainer}>
           <div className={styles.logo}>
             <img src="https://raw.githubusercontent.com/mindDrift/mind-drift-fe/dev/src/assets/mindDriftIcon.png" alt="mindDrift logo" />

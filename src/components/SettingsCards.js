aaaea8932515d849e1f85316d0d1775e7836d9ce
Dave Trost <davetrost@gmail.com>
2019-12-12 11:28:18 -0800

    use arrows to show cycle on settings cards

--- src/components/SettingsCards.js
+++ src/components/SettingsCards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './SettingsCards.css';
+import arrowIcon from '../assets/arrowicon_green.png';
 
 const SettingsCards = ({ settingsList, handleSelectSettings, handleEdit, handleBreatheNow, selectedId }) => {
   const settingsElements = settingsList.map(({ 
@@ -16,21 +17,48 @@ const SettingsCards = ({ settingsList, handleSelectSettings, handleEdit, handleB
   }) => {
     const editable = !userId.includes('__default__');
     const selected = selectedId === _id;
+    const cycles = Math.floor(endTime / (inhale + holdIn + exhale + holdOut));
     return (
-      <li 
+      <li
         className={`${styles.items} ${selected && styles.selected || ''}`}
         key={_id}
-        onClick={() => handleSelectSettings(_id)}>
+        onClick={() => handleSelectSettings(_id)}
+      >
         {selected && editable && <button name='editButton' onClick={() => handleEdit(_id)}><img src='https://raw.githubusercontent.com/mindDrift/mind-drift-fe/dev/src/assets/editICON.png' alt='edit'/></button>}
         <h3>{title}</h3>
         <p>{description}</p>
-        <ul>
-          <li>Inhale: {inhale}</li>
-          {holdIn > 0 && <li>hold: {holdIn}</li>}
-          <li>Exhale: {exhale}</li>
-          {holdOut > 0 && <li>Hold: {holdOut}</li>}
-        </ul>
-        <p>Continue for {endTime} seconds</p>
+        <div name='breathingSettingsContainer'>
+          <div name='breathingSettingsFlex'>
+            <ul>
+              <li>Inhale</li>
+              <li>{inhale}</li>
+            </ul>
+            <img src={arrowIcon} />
+            {holdIn > 0 && 
+              <>
+                <ul>
+                  <li>Hold</li>
+                  <li>{holdIn}</li>
+                </ul>
+                <img src={arrowIcon} />
+              </>
+            }
+            <ul>
+              <li>Exhale</li>
+              <li>{exhale}</li>
+            </ul>
+            {holdOut > 0 && 
+              <>
+                <img src={arrowIcon} />
+                <ul>
+                  <li>Hold</li>
+                  <li>{holdOut}</li>
+                </ul>
+              </>
+            }
+          </div>
+        </div>
+        <p>Repeat {cycles} times</p>
         {selected && <button name='breatheButton' onClick={handleBreatheNow}>Breathe now</button>}
       </li>
     );

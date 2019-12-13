11bd9f5b794fa671c230561e7365ee9a8a42c051
Abbey Masters <abbeymasters@Abbeys-MBP.localdomain>
2019-12-13 10:40:57 -0800

    fixed transition on about us page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -22,26 +22,26 @@ const AboutUs = () => {
         className={styles.logo}
         style={props}>
         <img src={brownFlowerIcon} />
-        <p>mindDrift is a mindful breathing app, aimed to make your experience seamless and convenient.</p>
+        <p className={styles.blurb}>mindDrift is a mindful breathing app, aimed to make your experience seamless and convenient.</p>
+        <section className={styles.AboutUs}>
+          <h1>Meet Our Team:</h1>
+          <div>
+            <img src={abbeyImg} alt="Abbey Masters" />
+            <h2>Abbey Masters</h2>
+            <p>Abbey is a Full-Stack developer, and enjoys long walks at the park with her dog. And also chocolate chip cookies!</p>
+          </div>
+          <div>
+            <img src={daveImg} alt="Dave Trost" />
+            <h2>Dave Trost</h2>
+            <p>Dave is a Back-End developer who is surprised by how much he likes React and Redux. He is a big nerd for disc golf, and lots of other things too. Lots of GF chocolate chip cookies!</p>
+          </div>
+          <div>
+            <img src={maeveImg} alt="Maeve Griffin" />
+            <h2>Maeve Griffin</h2>
+            <p>Maeve is a Full-Stack Javascript developer who has learned a lot about React Spring.  And likes trying new chocolate chip cookie recipes.</p>
+          </div>
+        </section>
       </animated.div>
-      <section className={styles.AboutUs}>
-        <h1>Meet Our Team:</h1>
-        <div>
-          <img src={abbeyImg} alt="Abbey Masters" />
-          <h2>Abbey Masters</h2>
-          <p>Abbey is a Full-Stack developer, and enjoys long walks at the park with her dog. And also chocolate chip cookies!</p>
-        </div>
-        <div>
-          <img src={daveImg} alt="Dave Trost" />
-          <h2>Dave Trost</h2>
-          <p>Dave is a Back-End developer who is surprised by how much he likes React and Redux. He is a big nerd for disc golf, and lots of other things too. Lots of GF chocolate chip cookies!</p>
-        </div>
-        <div>
-          <img src={maeveImg} alt="Maeve Griffin" />
-          <h2>Maeve Griffin</h2>
-          <p>Maeve is a Full-Stack Javascript developer who has learned a lot about React Spring.  And likes trying new chocolate chip cookie recipes.</p>
-        </div>
-      </section>
     </>
   );
 };

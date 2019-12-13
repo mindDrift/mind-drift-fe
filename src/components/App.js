5f06faf2343144e07c02955dfea4ade1dc99614e
Dave Trost <davetrost@gmail.com>
2019-12-12 17:23:20 -0800

    integrate firebase with app

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { useAuth0 } from '../react-auth0-spa';
-import PrivateRoute from './PrivateRoute';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Profile from '../containers/Profile';
 import Session from '../containers/Session';
 import Settings from '../containers/Settings';
 import EditSettings from '../containers/EditSettings';
 import styles from './App.css';
-import Loading from './Loading';
 import AboutUs from './AboutUs';
+import { withSession } from '../utils/WithSession';
 
-const App = () => {
-  const { loading } = useAuth0();
-
-  if(loading) {
-    return (<Loading loading={loading} />);
-  }
-
-  return (
-    <div className={styles.container}>
-      <Router>
-        <Switch>
-          <PrivateRoute exact path='/' component={Home} />
-          <PrivateRoute path='/profile' component={Profile} />
-          <PrivateRoute path='/breathe' component={Session} />
-          <PrivateRoute path='/settings' component={Settings} />
-          <PrivateRoute path='/edit-settings' component={EditSettings} />
-          <PrivateRoute path='/about-us' component={AboutUs} />
-        </Switch>
-      </Router>
-    </div>
-  );
-};
+const App = () => (
+  <div className={styles.container}>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={withSession(Home)} />
+        <Route path='/profile' component={withSession(Profile)} />
+        <Route path='/breathe' component={withSession(Session)} />
+        <Route path='/settings' component={withSession(Settings)} />
+        <Route path='/edit-settings' component={withSession(EditSettings)} />
+        <Route path='/about-us' component={withSession(AboutUs)} />
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'regenerator-runtime/runtime';
 import App from './components/App';
-import { Auth0Provider } from './react-auth0-spa';
-import config from './auth_config.json';
-import history from './utils/history';
 import { Provider } from 'react-redux';
 import store from './store';
 import './reset.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const onRedirectCallback = appState => {
-  history.push(
-    appState && appState.targetUrl ?
-      appState.targetUrl :
-      window.location.pathname
-  );
-};
+import { AuthProvider } from './utils/WithSession';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Auth0Provider
-      domain={config.domain}
-      client_id={config.clientId}
-      redirect_uri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
-    >
+    <AuthProvider>
       <App />
-    </Auth0Provider>
+    </AuthProvider>
   </Provider>,
   document.getElementById('root')
 );

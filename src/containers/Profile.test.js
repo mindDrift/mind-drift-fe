f52b59bf1b0be62380373324464b4d4c37b23aba
Dave Trost <davetrost@gmail.com>
2019-12-12 22:14:27 -0800

    lots of mocks in Profile test but the PropTypes is *still* failing

--- src/containers/Profile.test.js
+++ src/containers/Profile.test.js
@@ -5,14 +5,31 @@ import Profile from './Profile';
 import withSessionMock from '../utils/WithSession';
 jest.mock('../utils/WithSession', () => ({}));
 
+import achievementServicesMock from '../services/achievement';
+jest.mock('../services/achievement', () => ({}));
+
+import sessionServicesMock from '../services/session';
+jest.mock('../services/session', () => ({}));
+
+import userServicesMock from '../services/user';
+jest.mock('../services/user', () => ({}));
+
 describe('Profile component', () => {
+  withSessionMock.useSession = () => ({
+    user: {
+      displayName: 'me',
+      photoURL: 'image_url',
+    }
+  });
+
+  achievementServicesMock.fetchAchievements = () => ([]);
+
+  sessionServicesMock.getAverage = () => [60];
+  sessionServicesMock.getTotal = () => [12345];
+
+  userServicesMock.fetchUser = () => [{ currentStreak: 5 }];
+
   it('renders the profile', () => {
-    withSessionMock.useSession = () => ({
-      user: {
-        displayName: 'me',
-        photoURL: 'image_url',
-      }
-    });
     const wrapper = shallow(<Profile />);
     expect(wrapper).toMatchSnapshot();
   });

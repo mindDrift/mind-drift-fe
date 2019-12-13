7a9a50aece567e52703c4b6123086ab6064ee3f6
Dave Trost <davetrost@gmail.com>
2019-12-12 17:23:53 -0800

    fix 2 components to be compatible with firebase

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import { useAuth0 } from '../react-auth0-spa';
 import { Link } from 'react-router-dom';
 import styles from './NavBar.css';
 import HOMEicon from '../assets/HOMEicon.png';
 import PROFILEicon from '../assets/PROFILEicon.png';
 import SETTINGSicon from '../assets/SETTINGSicon.png';
 import FLOWERicon from '../assets/FLOWERicon.png';
+import { useLogout } from '../utils/WithSession';
+
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const logout = useLogout();
 
   return (
     <div className={styles.NavBar}>
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log In</button>
-      )}
-      {isAuthenticated && <button onClick={() => logout()}>Log Out</button>}
-      {isAuthenticated && (
-        <span className={styles.NavigationFooter}>
-          <Link to="/"><img src={HOMEicon} alt="Home" /></Link>&nbsp;
-          <Link to="/profile"><img src={PROFILEicon} alt="Profile" /></Link>
-          <Link to="/settings"><img src={SETTINGSicon} alt="Settings" /></Link>
-          <Link to="/about-us"><img src={FLOWERicon} alt="About Us" /></Link>
-        </span>
-      )}  
+      <button onClick={logout}>Log Out</button>
+      <span className={styles.NavigationFooter}>
+        <Link to="/"><img src={HOMEicon} alt="Home" /></Link>&nbsp;
+        <Link to="/profile"><img src={PROFILEicon} alt="Profile" /></Link>
+        <Link to="/settings"><img src={SETTINGSicon} alt="Settings" /></Link>
+        <Link to="/about-us"><img src={FLOWERicon} alt="About Us" /></Link>
+      </span>
     </div>
   );
 };
